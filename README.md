e7d6aa0bf7c791c38643eb4b2e3852aec1638549
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-09-19 18:32:58 +0300

    Setup handlebars

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ JS Back-End Course Workshop @SoftUni
 ## Developing Steps
  1. Initial Project Architecture
  2. Setup express
+ 3. Setup handlebars
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('Just works');
+    res.render('index', { layout: false });
 });
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
