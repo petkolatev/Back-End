68d990c0e4f8464234fd5eda39c250b1ccba0541
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-09-19 19:13:27 +0300

    Add routes module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 
-import homeController from './controllers/homeController.js';
+import routes from './routes.js';
 
 const app = express();
 
@@ -13,6 +13,6 @@ app.set('views', './src/views');
 
 app.use(express.static('public'));
 
-app.use(homeController);
+app.use(routes);
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import homeController from './controllers/homeController.js';
+
+const router = Router();
+
+router.use(homeController);
+
+export default router;
