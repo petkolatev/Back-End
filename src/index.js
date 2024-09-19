b8d6e00d348db8910bc4251d1713afe5322aa195
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-09-19 18:27:23 +0300

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Just works!!!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Just works');
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
